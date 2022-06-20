70255071fdb3d4c1c5289f60b9ce5fd8e45a5ab8
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2022-06-20 17:07:15 -0300

    RETORNADO A VERSÃO MAIS ESTAVEL

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Sobre from '../src/components/sobre';
 import Skills from '../src/components/Skills';
 import Projetos from '../src/components/projetos';
 import Footer from '../src/components/footer';
-import Script from 'next/script';
 
 const Root = styled.div`
 
@@ -16,9 +15,6 @@ const Root = styled.div`
 export default function Home() {
   return (
     <Root>
-              <!-- Global site tag (gtag.js) - Google Analytics -->
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-D5GEC46GEM"></Script>
-        <Script> window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-D5GEC46GEM'); </Script>
       <Navbar />
       <Resumo />
       <Sobre />
