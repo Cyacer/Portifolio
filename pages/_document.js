b85e539f9390821def5a99dcf94306f931b7a39f
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2022-06-20 16:51:55 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,63 +1,28 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-props-no-spreading */
-import Document from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-import { GA_TRACKING_ID } from '../lib/gtag';
+import { Head } from 'next/document';
 
 export default class MyDocument extends Document {
-
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-
-  }
-}
   render() {
-        return (
-            <Html lang="en">
+    return (
+      // ...
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=[Tracking ID]"
+        />
 
-                {/* other code */}
-
-            <Head>
-                    {/* Global Site Tag (gtag.js) - Google Analytics */}
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-                        }}
-                    />
-            </Head>
-            
-                    </Html>
-        )
-    }
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '[Tracking ID]', { page_path: window.location.pathname });
+            `,
+          }}
+        />
+      </Head>
+    );
+  }
 }
