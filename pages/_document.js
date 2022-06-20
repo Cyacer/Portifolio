e7de42ef89e6d5e1a0b77e4e70c50d4bdb7c457a
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2022-06-20 16:48:49 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,33 @@ import { ServerStyleSheet } from 'styled-components';
 import { GA_TRACKING_ID } from '../lib/gtag';
 
 export default class MyDocument extends Document {
-    render() {
+
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+
+  }
+}
+  render() {
         return (
             <Html lang="en">
 
@@ -30,31 +56,8 @@ export default class MyDocument extends Document {
                         }}
                     />
             </Head>
-
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
+            
+                    </Html>
+        )
     }
-           </Html>
-           )
-           }
-  }
 }
