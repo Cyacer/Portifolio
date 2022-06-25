c6597c4b5c65986e00e1efc453ffb9dd1911b0d0
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2022-06-25 02:18:37 -0300

    Alteração nas Skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -18,22 +18,13 @@ const IconsSkills = styled.div`
 @media (min-width: 1024px ){  
 
 display:grid;
-grid-template-columns: 30% 70% ;
+grid-template-columns: 100% ;
 justify-content:center;
 align-items:center;
 font-size:72px;
 
 }
 
-`;
-const Description = styled.p`
-margin-bottom:24px;
-@media (min-width: 1024px ){
-font-size:1.17rem;
-align-items:none;
-justify-content:none;
-padding-left:20px;
-}
 `;
 
 export default function Skills() {
@@ -43,7 +34,6 @@ export default function Skills() {
         Skills
       </Categoria>
       <IconsSkills>
-        <Description>*passe o cursor do mouse no card para ler* </Description>
         <SkillsIcons />
       </IconsSkills>
     </Container>
